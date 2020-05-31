c1794b956d68f166cc750b47a76c8e00b9ce2a28
Michał Darda <michaldarda@gmail.com>
2020-05-31 15:42:28 +0200

    Add maven and junit5

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class JUnit5ExampleTest {
+
+    @Test
+    void justAnExample() {
+        assertEquals(1, 2);
+    }
+}
