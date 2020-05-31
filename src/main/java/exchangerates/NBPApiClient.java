ee274e0ab1498ccdcc000be6e859da2933c9c7ca
aciesielczuk <56974114+aciesielczuk@users.noreply.github.com>
2020-05-31 16:41:21 +0200

    implemented getExchcange method

--- src/main/java/exchangerates/NBPApiClient.java
+++ src/main/java/exchangerates/NBPApiClient.java
@@ -1,7 +1,33 @@
 package exchangerates;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class NBPApiClient {
-    public String getExchangeRates(String a, int i) {
-        return "OK";
+
+    public String getExchangeRates(String a, int i) throws IOException {
+        URL url = new URL("http://api.nbp.pl/api/exchangerates/tables/C/last/10");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Accept", "application/xml");
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        con.disconnect();
+
+
+        return content.toString();
     }
+
+
 }
--- src/test/java/exchangerates/NBPApiClientTest.java
+++ src/test/java/exchangerates/NBPApiClientTest.java
@@ -1,14 +1,19 @@
 package exchangerates;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NBPApiClientTest {
 
     @Test
-    void returnsXML() {
+    void returnsXML() throws IOException {
         NBPApiClient nbpApiClient = new NBPApiClient();
         String result = nbpApiClient.getExchangeRates("A", 1);
-        assertEquals("OK", result);
+       // assertEquals("OK", result);
+        assertThat(result).contains("ArrayOfExchangeRatesTable");
     }
 }
